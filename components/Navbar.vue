1c8a3af9994a1d0f4a47002d53c0745625ad4158
Alexander <kharchenko7717@gmail.com>
2020-06-20 13:58:19 +0300

    update: tabs

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    	<a class="navbar-brand">Sneakersale Task</a>
-    	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-    		<span class="navbar-toggler-icon"></span>
-    	</button>
-    	<div class="collapse navbar-collapse" id="navbarNav">
-    		<ul class="navbar-nav">
-    			<li v-for='page in routes' :key='page.name' class="nav-item">
+        <a class="navbar-brand">Sneakersale Task</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li v-for='page in routes' :key='page.name' class="nav-item">
                     <nuxt-link active-class='active' class='nav-link' :to='page.link' exact>
-		                {{page.name}}
+                        {{page.name}}
                     </nuxt-link>
-    			</li>
-    		</ul>
-    	</div>
+                </li>
+            </ul>
+        </div>
     </nav>
 </template>
 
